08c8655e6cfd7de2fc5a1076d7d8e98b00a79cfc
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-02-28 01:40:18 +0200

    3

--- lib/constans/colors_constans.dart
+++ lib/constans/colors_constans.dart
@@ -1,16 +1,21 @@
-
-
 import 'package:flutter/material.dart';
 
-const MaterialColor primarySwatchColor =  MaterialColor(0xFF38426B,  <int, Color>{
-  50: Color.fromRGBO(56, 66, 107, .1),
-  100: Color.fromRGBO(56, 66, 107, .2),
-  200: Color.fromRGBO(56, 66, 107, .3),
-  300: Color.fromRGBO(56, 66, 107, .4),
-  400: Color.fromRGBO(56, 66, 107, .5),
-  500: Color.fromRGBO(56, 66, 107, .6),
-  600: Color.fromRGBO(56, 66, 107, .7),
-  700: Color.fromRGBO(56, 66, 107, .8),
-  800: Color.fromRGBO(56, 66, 107, .9),
-  900: Color.fromRGBO(56, 66, 107, 1),
-});
+//Базированный цвет. Это база????
+const MaterialColor primarySwatchColor = MaterialColor(
+  _primarySwatchColor,
+  <int, Color>{
+    50: Color(0xffb7c5ff),
+    100: Color(0xFF636FD4),
+    200: Color(0xFF3E4C99),
+    300: Color(0xFF6A78A3),
+    350: Color(0xFF606B9C), // only for raised button while pressed in light theme
+    400: Color(0xFF4B5587),
+    500: Color(_primarySwatchColor),
+    600: Color(0xFF2A3357),
+    700: Color(0xFF1A2249),
+    800: Color(0xFF151E45),
+    850: Color(0xFF090D32), // only for background color in dark theme
+    900: Color(0xFF040924),
+  },
+);
+const int _primarySwatchColor = 0xFF38426B;
